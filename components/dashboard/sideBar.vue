5746ac14a4cd054fb7a8fa2f621c359008d2a728
mehranyosefi <u3efimehran@gmail.com>
2025-03-10 19:59:13 +0330

    create custome sidebar component for dashboard insted of dock primevue

---
+++ components/dashboard/sideBar.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+defineProps<{
+  items: Array<any>;
+}>();
+
+const { locale, t } = useI18n();
+const localePath = useLocalePath();
+const useWindow = useWindowProperty();
+const appStore = useAppStore();
+
+const dockPosition = computed(() => {
+  if (useWindow.isOnMobile.value) return "bottom";
+  else if (locale.value === "en") return "left";
+  else return "right";
+});
+</script>
+
+<template>
+  <nav
+    class="fixed bottom-0 ltr:left-0 rtl:right-0 w-full xs:top-0 xs:w-fit xs:h-screen shadow-2xl"
+    :dir="appStore.dir"
+  >
+    <slot name="prepend"></slot>
+    <ul class="p-0 m-0 list-none flex xs:flex-col">
+      <li v-for="(item, index) in items" :key="index" class="grow">
+        <slot name="item" :item="item">
+          <PrimeButton
+            :as="item.path ? 'router-link' : 'button'"
+            :to="localePath(item.path)"
+            class="w-full! rounded-none! border-none! flex flex-col lg:flex-row items-center justify-center lg:justify-start! sm:py-4! hover:bg-gray-200! dark:hover:bg-gray-800! group"
+            :name="item.label as string"
+            :pt="{
+              root: 'py-2! ',
+            }"
+            outlined
+            v-tooltip="
+              dockPosition !== 'bottom' && useWindow.isOnTablet.value
+                ? t(item.label)
+                : ''
+            "
+            activeClass="dashboard-link-active"
+            exactActiveClass="dashboard-link-exact-active"
+          >
+            <svg
+              class="route-link__icon size-8 text-gray-400 group-hover:text-green-500 dark:group-hover:text-primary-500"
+            >
+              <use class="size-8" :href="item.icon"></use>
+            </svg>
+            <span
+              class="text-gray-900 dark:text-gray-100 text-lg font-semibold xs:hidden lg:block"
+              >{{ t(item.label as string) }}</span
+            >
+          </PrimeButton>
+        </slot>
+      </li>
+    </ul>
+    <slot name="append"></slot>
+  </nav>
+</template>
--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -4,7 +4,6 @@ import { dashboardMenu } from "~/types";
 
 const { locale, t } = useI18n();
 const useApp = useAppStore();
-const localePath = useLocalePath();
 
 useHead({
   titleTemplate: `${t("general.UptimeRobot")} - %s`,
@@ -27,11 +26,6 @@ withDefaults(
 );
 
 const useWindow = useWindowProperty();
-const dockPosition = computed(() => {
-  if (useWindow.isOnMobile.value) return "bottom";
-  else if (locale.value === "en") return "left";
-  else return "right";
-});
 const sideBarItems = computed(() => {
   if (useWindow.isOnMobile.value) {
     return dashboardMenu.slice(0, 4);
@@ -71,46 +65,7 @@ const conversation: ShallowRef<boolean> = shallowRef<boolean>(false);
     </div>
 
     <ClientOnly>
-      <PrimeDock
-        :model="sideBarItems"
-        :pt="{
-          root: 'shadow-2xl w-16 lg:w-52',
-          listContainer: `p-0! rounded-none! h-full justify-start! w-full border-none!`,
-          list: 'w-full h-full flex! justify-start!',
-          item: 'w-full p-0!',
-        }"
-        :position="dockPosition"
-      >
-        <template #item="{ item }">
-          <PrimeButton
-            :as="item.path ? 'router-link' : 'button'"
-            :to="localePath(item.path)"
-            class="w-full! rounded-none! border-none! flex flex-col lg:flex-row items-center justify-center lg:justify-start! sm:py-4! hover:bg-gray-200! dark:hover:bg-gray-800! group"
-            :name="item.label as string"
-            :pt="{
-              root: 'py-2! ',
-            }"
-            outlined
-            v-tooltip="
-              dockPosition !== 'bottom' && useWindow.isOnTablet.value
-                ? t(item.label)
-                : ''
-            "
-            activeClass="dashboard-link-active"
-            exactActiveClass="dashboard-link-exact-active"
-          >
-            <svg
-              class="route-link__icon size-8 text-gray-400 group-hover:text-green-500 dark:group-hover:text-primary-500"
-            >
-              <use class="size-8" :href="item.icon"></use>
-            </svg>
-            <span
-              class="text-gray-900 dark:text-gray-100 text-lg font-semibold xs:hidden lg:block"
-              >{{ t(item.label as string) }}</span
-            >
-          </PrimeButton>
-        </template>
-      </PrimeDock>
+      <DashboardSideBar :items="sideBarItems"></DashboardSideBar>
     </ClientOnly>
     <UiChat
       class="chat"
